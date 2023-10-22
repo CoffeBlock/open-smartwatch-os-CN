b3befb0521e3d9a95ee07d437b710781c128fd4d
CoffeBlock <coffeblock@coffeblock.com>
2023-10-22 14:01:46 -0700

    modified:   .vscode/launch.json 	new file:   include/locales/cn-ZH.h

---
+++ include/locales/cn-ZH.h
@@ -0,0 +1,78 @@
+#ifndef LANG_ZH_H
+#define LANG_ZH_H
+
+// General terms
+#define LANG_CONNECT "连接"
+#define LANG_CONNECTED "已连接"
+#define LANG_DISCONNECT "失去连接"
+
+// App: Time from web
+#define LANG_TFW_UPDATE "更新"
+#define LANG_MANUALLY "手动"
+#define LANG_CANCEL "取消"
+#define LANG_SAVE "保存"
+
+// App: Alarm
+#define LANG_ALARM "闹钟"
+#define LANG_NEXT "下一个"
+#define LANG_DAILY "每天"
+#define LANG_WEEKDAYS "工作日"
+#define LANG_WEEKENDS "周末"
+
+// App: Timer
+#define LANG_TIMER "计时"
+#define LANG_START "开始"
+
+// App: BLE media control
+#define LANG_BMC_VOLUME "音量"
+#define LANG_BMC_CONNECTING "连接中..."
+
+// App: Webserver
+#define LANG_WEBSRV_STATION_PWD "Station Password:"
+#define LANG_WEBSRV_TITLE "Configuration"
+#define LANG_WEBSRV_USER "User:"
+#define LANG_WEBSRV_PASS "Password:"
+#define LANG_WEBSRV_AP_PASSWORD_ON "AutoAP PW ON"
+#define LANG_WEBSRV_AP_PASSWORD_OFF "AutoAP PW OFF"
+
+// App: Watchface Binary
+#define LANG_WATCHFACE_BINARY_STEPS "步数"
+
+// App: Watchface Fitness
+#define LANG_WATCHFACE_FITNESS_DISTANCE "距离"
+#define LANG_WATCHFACE_FITNESS_STEP "步数"
+
+// App: Fitness Statistics
+#define LANG_FITNESS_TITLE "运动"
+#define LANG_FITNESS_STEP "步数"
+#define LANG_FITNESS_DISTANCE "距离"
+#define LANG_FITNESS_TOTAL "总计"
+
+// App: Step Statistics
+#define LANG_STEPSTATS_TITLE "步数"
+
+// App: Kcal Statistics
+#define LANG_KCALSTATS_TITLE "卡路里"
+
+// App: Distance Statistics
+#define LANG_DISTSTATS_TITLE "距离"
+
+// Weekdays
+#define LANG_SUNDAY "周日"
+#define LANG_MONDAY "周一"
+#define LANG_TUESDAY "周二"
+#define LANG_WEDNESDAY "周三"
+#define LANG_THURSDAY "周四"
+#define LANG_FRIDAY "周五"
+#define LANG_SATURDAY "周六"
+
+// Weekdays (short)
+#define LANG_SUN "周日"
+#define LANG_MON "周一"
+#define LANG_TUE "周二"
+#define LANG_WED "周三"
+#define LANG_THU "周四"
+#define LANG_FRI "周五"
+#define LANG_SAT "周六"
+
+#endif
